55614d6cab4d8a78873fb5dc4926ab39a6122827
Abhishek Patel <abhishek@HP-Spectre.localdomain>
2022-11-22 16:49:45 -0600

    UI Skeleton Ready

---
+++ gui.py
@@ -0,0 +1,20 @@
+
+import sys
+
+from PyQt6 import QtCore, QtGui
+from PyQt6.QtWidgets import *
+from form import Ui_Form
+
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.ui = Ui_Form()
+        self.ui.setupUi(self)
+
+if __name__ == "__main__":
+
+    app = QApplication(sys.argv)
+    MyWindow = MainWindow()
+    MyWindow.show()
+    sys.exit(app.exec())
--- stringmatching.py
+++ stringmatching.py
@@ -1,8 +1,8 @@
 ## String matching Algorithms
 ## Robin-Karp ALgorithm
 ## Knuth-Morris-Pratt Algorithm
-
-
+import os
+import time
 class StringMatching:
     def __init__(self,text,pattern, prime_number = 13):
         self.text = text
@@ -23,6 +23,9 @@ class StringMatching:
         j = 0
         h = 1
         counter = 0
+        iterator_outter_loop = 0
+        iterator_inner_loop = 0
+        location = []
         for i in range(self.pattern_length-1):
             h = (h*self.text_length)
 
@@ -32,18 +35,20 @@ class StringMatching:
             self.hash_pattern = (self.text_length * self.hash_pattern + ord(self.pattern[i])) % self.prime_number
             self.hash_text = (self.text_length * self.hash_text + ord(self.text[i])) % self.prime_number
 
-
+        
         for i in range(self.text_length - self.pattern_length + 1):
+            iterator_outter_loop += 1
             if self.hash_pattern == self.hash_text:
 
                 for j in range(self.pattern_length):
+                    iterator_inner_loop += 1
                     if self.text[i+j]  != self.pattern[j]:
                         break
                     else:
                         j += 1
                 if j == self.pattern_length:
                     counter += 1
-                    print("Pattern found at index " + str(i))
+                    location.append(i)
             if i < self.text_length-self.pattern_length:
                 self.hash_text = (self.text_length *(self.hash_text - ord(self.text[i])*h) + ord(self.text[i+self.pattern_length])) % self.prime_number
 
@@ -51,18 +56,28 @@ class StringMatching:
                     self.hash_text = self.hash_text + self.prime_number
         
         print(f"Pattern found {counter} times.")
+        # print(f"Pattern found on loactions: {location}.")
+        print(f"Total number of loop iterations: {iterator_outter_loop+iterator_inner_loop}.")
+        print(f"Expected Best or Average case time complexity:{self.pattern_length + self.text_length}.")
+        print(f"Expected Worst case time complexity:{self.pattern_length * self.text_length}.")
 
 
 
 if __name__ == '__main__':
 
-    file = open("./1960-09-26.txt").readlines()
+    data_root = './presidential_debates'
     data = []
-    for s in file:
-        data.append(s.rstrip('\n')) 
+    for filename in os.listdir(data_root):
+        file = open(os.path.join(data_root,filename), encoding='UTF-8')
+        doc = file.readlines()
+        for s in doc:
+            data.append(s) 
     text = "".join(data)
 
     pattern = "President"
     q = 11
-    SM = StringMatching(text,pattern)
+    SM = StringMatching(text,pattern,q)
+    start_time = time.time()
     SM.RobinKarp()
+    end_time = time.time()
+    print(f"Time required for Robin-Karp execution:{end_time-start_time} sec.")
