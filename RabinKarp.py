28b7d51af7cfccd594cf304cfe8b0a50adb6328c
Abhishek Patel <abhishek@HP-Spectre.localdomain>
2022-11-30 10:09:56 -0600

    Finalized Rabin-Karp-Implementation

--- RabinKarp.py
+++ RabinKarp.py
@@ -1,9 +1,8 @@
-## String matching Algorithms
-## Robin-Karp ALgorithm
-## Knuth-Morris-Pratt Algorithm
+## String matching Algorithm
+## Rabin-Karp ALgorithm
 
-
-class StringMatching:
+import time
+class RabinKarp:
     def __init__(self,text,pattern, prime_number = 13):
         self.text = text
         self.pattern = pattern
@@ -17,12 +16,15 @@ class StringMatching:
         self.hash_text = 0    # hash value for txt
 
     
-    def RobinKarp(self):
+    def search(self):
 
         i = 0
         j = 0
         h = 1
         counter = 0
+        iterator_outter_loop = 0
+        iterator_inner_loop = 0
+        location = []
         for i in range(self.pattern_length-1):
             h = (h*self.text_length)
 
@@ -32,37 +34,45 @@ class StringMatching:
             self.hash_pattern = (self.text_length * self.hash_pattern + ord(self.pattern[i])) % self.prime_number
             self.hash_text = (self.text_length * self.hash_text + ord(self.text[i])) % self.prime_number
 
-
+        
         for i in range(self.text_length - self.pattern_length + 1):
+            iterator_outter_loop += 1
             if self.hash_pattern == self.hash_text:
 
                 for j in range(self.pattern_length):
+                    iterator_inner_loop += 1
                     if self.text[i+j]  != self.pattern[j]:
                         break
                     else:
                         j += 1
                 if j == self.pattern_length:
                     counter += 1
-                    print("Pattern found at index " + str(i))
+                    location.append(i)
             if i < self.text_length-self.pattern_length:
                 self.hash_text = (self.text_length *(self.hash_text - ord(self.text[i])*h) + ord(self.text[i+self.pattern_length])) % self.prime_number
 
                 if self.hash_text < 0:
                     self.hash_text = self.hash_text + self.prime_number
         
+        # return (counter,iterator_inner_loop+iterator_outter_loop,self.pattern_length + self.text_length,self.pattern_length * self.text_length,location)
         print(f"Pattern found {counter} times.")
+        # print(f"Pattern found on loactions: {location}.")
+        print(f"Total number of loop iterations: {iterator_outter_loop+iterator_inner_loop}.")
+        print(f"Expected Best or Average case time complexity:{self.pattern_length + self.text_length}.")
+        print(f"Expected Worst case time complexity:{self.pattern_length * self.text_length}.")
 
 
 
 if __name__ == '__main__':
+    
+    file = open("The Complete Works of William Shakespeare.txt", "r", encoding='UTF-8')
+    doc = file.readlines() 
+    text = "".join(doc)
 
-    file = open("./1960-09-26.txt").readlines()
-    data = []
-    for s in file:
-        data.append(s.rstrip('\n')) 
-    text = "".join(data)
-
-    pattern = "President"
+    pattern = "Book"
     q = 11
-    SM = StringMatching(text,pattern)
-    SM.RobinKarp()
+    SM = RabinKarp(text,pattern,q)
+    start_time = time.time()
+    SM.search()
+    end_time = time.time()
+    print(f"Time required for Robin-Karp execution:{end_time-start_time} sec.")
