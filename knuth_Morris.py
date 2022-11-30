971710438053b9623005984a2d5e5be5c816ec43
Thota Akhila <akhilathota929@gmail.com>
2022-11-30 11:09:15 -0600

    Oop code

--- knuth_Morris.py
+++ knuth_Morris.py
@@ -1,36 +1,40 @@
-def compute_prefix_fun(pattern):
-    m=len(pattern)
-    pre=[0]*m
-    k=0
-    for q in range(1,m):
-        while k and pattern[k]!=pattern[q]:
-            k=pre[k-1]
-        if pattern[k]==pattern[q]:
-            k=k+1 
-        pre[q]=k
-    return pre
+class KMP:
+    def __init__(self,text,pattern):
+        self.text = text
+        self.pattern = pattern
+        self.pattern_length = len(self.pattern)
+        self.text_length = len(self.text)
+
+    def compute_prefix_fun(self):
+        k=0
+        prefix = [0]*self.pattern_length
+        for i in range(1,self.pattern_length):
+            while k and self.pattern[k]!=self.pattern[i]:
+                k=prefix[k-1]
+            if self.pattern[k]==self.pattern[i]:
+                k=k+1 
+            prefix[i]=k
+        return prefix
 
 
-def kmp(pattern,text):
-    n= len(text)
-    m=len(pattern)
-    pre=compute_prefix_fun(pattern)
-    q=0
-    match_loc = []
-    for i in range(n):
-        # print("match locations: ",match_loc)
-        while q and pattern[q]!=text[i]:
-            q=pre[q-1]
-        if pattern[q]==text[i]:
-            if q==m-1:
-                match_loc.append(i-q)
-                q = pre[q]
-            else:
-                q=q+1
-    return match_loc
+    def search(self):
+        prefix=self.compute_prefix_fun()
+        q=0
+        match_loc = []
+        for i in range(self.text_length):
+            while q and self.pattern[q]!=self.text[i]:
+                q=prefix[q-1]
+            if self.pattern[q]==self.text[i]:
+                if q==self.pattern_length-1:
+                    match_loc.append(i-q)
+                    q = prefix[q]
+                else:
+                    q=q+1
+        return match_loc
 
-            
-text="aaaaaaaa"
-pattern="aa"
-k=kmp(pattern,text)
-print(k)
+
+if __name__ == '__main__':        
+    text="aaaaaaaa"
+    pattern="aa"
+    k=KMP(text,pattern)
+    print(k.search())s
